e6eb1a87156e940b102b2dcb67842daceaf117e9
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2021-09-18 22:31:04 +0900

    EEW受信基礎部分、強震モニタ画像、PS波表示の実装、ウィンドウサイズ微調整、デフォルト背景画像の設定

--- KyoshinMonitor EEW Observer REV-2/Form1.Designer.cs
+++ KyoshinMonitor EEW Observer REV-2/Form1.Designer.cs
@@ -30,19 +30,11 @@ namespace KyoshinMonitor_EEW_Observer_REV_2
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(-2, -1);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(272, 97);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // timer1
             // 
@@ -50,24 +42,34 @@ namespace KyoshinMonitor_EEW_Observer_REV_2
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 404);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 394);
-            this.Controls.Add(this.label1);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(352, 404);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- KyoshinMonitor EEW Observer REV-2/Form1.cs
+++ KyoshinMonitor EEW Observer REV-2/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using NAudio;
+using System.Net.Http;
+using System.IO;
+using System.Net;
 
 namespace KyoshinMonitor_EEW_Observer_REV_2
 {
@@ -19,14 +24,60 @@ namespace KyoshinMonitor_EEW_Observer_REV_2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            DateTime dt = DateTime.Now;
-            label1.Text = dt.ToString("yyyy/MM/dd HH:mm:ss");
+
+
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private async void timer1_Tick(object sender, EventArgs e)
         {
-            DateTime dt = DateTime.Now;
-            label1.Text = dt.ToString("yyyy/MM/dd HH:mm:ss");
+            //EEW部分
+            {
+                DateTime dt = DateTime.Now; //現在時刻の取得(PC時刻より)
+                var tm = dt.AddSeconds(-2); //現在時刻から2秒引く(取得失敗を防ぐため)
+                var time = tm.ToString("yyyyMMddHHmmss");//時刻形式の指定(西暦/月/日/時/分/秒)
+                var client = new HttpClient();
+
+                var url = $"http://www.kmoni.bosai.go.jp/webservice/hypo/eew/{time}.json"; //強震モニタURLの指定
+
+                var json = await client.GetStringAsync(url); //awaitを用いた非同期JSON取得
+                var eew = JsonConvert.DeserializeObject<EEW>(json);//EEWクラスを用いてJSONを解析(デシリアライズ)
+                var rpt_t = eew.report_time;
+                var reg = eew.region_name;
+                var lati = eew.latitude;
+                var lotu = eew.longitude;
+                var canc_flg = eew.is_cancel;
+                var depth = eew.depth;
+                var intn = eew.calcintensity;
+                var end_flg = eew.is_final;
+                var rpt_no = eew.report_num;
+                var ori_t = eew.origin_time;
+                var al_flg = eew.alertflg;
+                var eew_flg = eew.result.message;
+                var mag = eew.magunitude;
+
+            }
+            //強震モニタ画像部分
+            {
+                try
+                {
+                    DateTime dt1 = DateTime.Now;
+                    var dt = dt1.AddSeconds(-2);
+                    var url1 = $"http://www.kmoni.bosai.go.jp//data/map_img/RealTimeImg/acmap_s/{dt.ToString("yyyyMMdd")}/{dt.ToString("yyyyMMddHHmmss")}.acmap_s.gif";
+                    WebClient wc1 = new WebClient();
+                    Stream stream1 = wc1.OpenRead(url1);
+                    Bitmap bitmap1 = new Bitmap(stream1);
+                    stream1.Close();
+                    bitmap1.MakeTransparent();
+                    pictureBox1.BackgroundImage = bitmap1;
+
+                    pictureBox1.ImageLocation = $"http://www.kmoni.bosai.go.jp/data/map_img/PSWaveImg/eew/{dt.ToString("yyyyMMdd")}/{dt.ToString("yyyyMMddHHmmss")}.eew.gif";
+                }
+                catch
+                {
+                    var aa = "at";
+                }
+            }
+
         }
     }
 }
